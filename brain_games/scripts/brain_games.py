74e4c24efcb3f2b78af075a64afde68a1eb7eab4
Daniel <shnoda39@gmail.com>
2025-09-02 15:31:36 +0000

    Init package hexlet-code with brain-games entrypoint

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,9 @@
+# brain_games/scripts/brain_games.py
+
+def main() -> None:
+    print("Welcome to the Brain Games!")
+
+
+if __name__ == "__main__":
+    # запуск в режиме: uv run python -m brain_games.scripts.brain_games
+    main()
