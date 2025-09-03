5e5f799158c85c9c09bb17dd0de464b00a9dc3d9
Daniel <shnoda39@gmail.com>
2025-09-03 09:51:46 +0000

    feat(games): add all entry scripts and unify game interface (DESCRIPTION, get_round)

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,11 @@
+# brain_games/scripts/brain_gcd.py
+from brain_games.engine import run
+from brain_games.games import gcd as game
+
+
+def main() -> None:
+    run(game)
+
+
+if __name__ == "__main__":
+    main()
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+# brain_games/scripts/brain_progression.py
+from brain_games.engine import run
+from brain_games.games import progression as game
+
+
+def main() -> None:
+    run(game)
+
+
+if __name__ == "__main__":
+    main()
