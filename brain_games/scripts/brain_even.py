920dd1f3646975ff2978fecb5b4588f9e6a19209
Daniel <shnoda39@gmail.com>
2025-09-03 09:15:50 +0000

    fix: include brain-even entry script and package

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+# brain_games/scripts/brain_even.py
+from brain_games.engine import run
+from brain_games.games import even
+
+
+def main() -> None:
+    run(even)
+
+
+if __name__ == "__main__":
+    main()
